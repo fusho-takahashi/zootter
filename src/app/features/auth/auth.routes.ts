1bc05006bd6b9f843338f65af03f2f0756126eab
fusho-takahashi <zenryaku@fusho-takahashi.me>
2024-11-30 06:38:45 +0900

    ESLintの設定を追加し、Angularプロジェクトにリント機能を統合

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import LoginPageComponent from './login/login-page.component';
 
-export default <Routes>[{ path: '', component: LoginPageComponent }];
+export default [{ path: '', component: LoginPageComponent }] as Routes;
