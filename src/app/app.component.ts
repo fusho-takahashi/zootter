669209f336a41b8ce47fd2b345dcfc4955438351
fusho-takahashi <zenryaku@fusho-takahashi.me>
2024-12-01 12:24:38 +0900

    ヘッダーにマテリアルデザインの区切り線を追加し、スタイルを改善

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { AuthStore } from './core/auth/auth.store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar],
+  imports: [RouterOutlet, MatToolbar, MatDivider],
   providers: [AuthStore],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
