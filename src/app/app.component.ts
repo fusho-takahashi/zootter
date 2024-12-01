6b945bd57831019c5125d515eb285a7d8de2be3f
fusho-takahashi <zenryaku@fusho-takahashi.me>
2024-12-01 09:02:21 +0900

    ログイン状態に応じてツールバーを表示する機能を追加。AuthStoreを使用してログイン状態を管理。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { AuthStore } from './core/auth/auth.store';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar],
   providers: [AuthStore],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly authStore = inject(AuthStore);
+
+  isLoggedIn = this.authStore.state.isLoggedIn;
+}
