300e85c9541a639771d3d4e21185882ebd7e40a6
fusho-takahashi <zenryaku@fusho-takahashi.me>
2024-12-14 16:14:59 +0900

    HTTPクライアントにロギングインターセプターを追加し、レスポンスのステータスをログに記録する機能を実装しました

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { AuthStore } from './core/auth/auth.store';
 import { CustomErrorHandler } from './core/handlers/custom-error-handler';
+import { loggingInterceptor } from './core/interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([loggingInterceptor])), // HttpClient を使っていないため、実際は動作しません。
     { provide: ErrorHandler, useClass: CustomErrorHandler },
     AuthStore,
   ],
